fac40401283f3f6ef67a3a0ce85deafcb7be1500
James-Cochran <jameslikesfruit@gmail.com>
2024-09-05 15:09:39 -0400

    Added patch route.  Added update action in the posters_controller. Passed Postman queries with success codes.

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,18 +1,23 @@
 class Api::V1::PostersController < ApplicationController
   def index
-    poster = Poster.all  # We're calling the poster model (singular)
+    posters = Poster.all  # We're calling the poster model (singular)
     render json: PosterSerializer.format_posters(posters)
   end
 
   def show
     poster = Poster.find(params[:id])
-    render json: PosterSerializer.format_posters(posters)
+    render json: PosterSerializer.format_poster(poster)
 
   end
 
   def create
     new_poster = Poster.create(poster_params)
     render json: PosterSerializer.format_poster(new_poster), status: 201
+  end
+
+  def update
+    updated_poster = Poster.update(params[:id], poster_params)
+    render json: PosterSerializer.format_poster(updated_poster)
   end
 
   private
